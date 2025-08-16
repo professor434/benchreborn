bccab51e6a3267eb50e93c9f3a97c8739c18bb23
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-17 02:27:47 +0300

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react";
 
-export default function Card({ className = "", children, ...props }) {
+export function Card({ className = "", children, ...props }) {
   return (
     <div
       className={`rounded-lg border border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-[#1d1d1d] ${className}`}
@@ -10,4 +10,12 @@ export default function Card({ className = "", children, ...props }) {
       {children}
     </div>
   );
-} 
+}
+
+export function CardContent({ children, className = "", ...props }) {
+  return (
+    <div className={className} {...props}>
+      {children}
+    </div>
+  );
+}
