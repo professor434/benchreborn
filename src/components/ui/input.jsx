1d93d17a01f2e687bf87436fc8f27cb23d8435ac
professor434 <81698289+professor434@users.noreply.github.com>
2025-08-17 02:29:55 +0300

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react";
 
-export default function Input({ className = "", ...props }) {
+export function Input({ className = "", ...props }) {
   return (
     <input
       className={`w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm
